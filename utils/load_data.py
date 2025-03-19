921320f4b8d6fa11cfa84ecb47bde80c723f0588
Callihan Bertley <callihanbertley@Callihans-MacBook-Pro-2.local>
2025-03-18 23:29:20 -0500

    Added test-train data splitting on load_housing_data function

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,6 +1,7 @@
 from torchvision.datasets import MNIST
 from torchvision.transforms import transforms
 from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
 import pandas as pd
 
 
@@ -21,12 +22,21 @@ def load_digits():
     return train_data, test_data
 
 def load_housing_data():
+    # Create df
     data_bunch = fetch_california_housing()
     col_names = data_bunch["feature_names"]
     data_arr = data_bunch["data"]
     df = pd.DataFrame(data=data_arr, columns=col_names)
-    return df
+    df["median_house_value"] = data_bunch["target"]
 
+
+    # train test split
+    X, y = df[df.columns[:-1]], df[df.columns[-1]]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    train_data = pd.concat([X_train, y_train], axis=1)
+    test_data = pd.concat([X_test, y_test], axis=1)
+    return train_data, test_data
 
 
 if __name__ == "__main__":
