dbea6922ac30484f7ef527f7c7432eb4d965e2f5
Callihan Bertley <callihanbertley@Callihans-MacBook-Pro-2.local>
2025-03-20 17:51:37 -0500

    Fixed error in try except construction, fixed error with torch autograd

--- models/regression.py
+++ models/regression.py
@@ -1,11 +1,13 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
 from utils.data_preprocessing import create_housing_data_loader
-
+import matplotlib.pyplot as plt
 
 
 class RegressionModel(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
+        super().__init__()
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
         self.output_dim = output_dim
@@ -24,24 +26,31 @@ class RegressionModel(nn.Module):
         return x
 
 
-def train_model(train_data, model, loss, optimizer, batch_size=64, error=10e-4):
-    train_loader = create_housing_data_loader(train_data, batch_size=batch_size, shuffle=True)
+def train_model(train_data, target, model, loss, optimizer, batch_size=64, error=10e-4):
+    train_loader = create_housing_data_loader(train_data, target=target, batch_size=batch_size, shuffle=True)
     epoch_losses = []
     epoch = 1
-    batch_loss = 0
+    total_batch_loss = 0
     prev_epoch_loss = float("inf")
 
-    
+
     while True:
         optimizer.zero_grad()
         try:
             train_batch = next(train_loader)
+            X, y = train_batch
+            y_pred = model(X)
+            batch_loss = loss(y, y_pred)
+            total_batch_loss += batch_loss.item()
+            batch_loss.backward()
+            optimizer.step()
 
-        except:
-            StopIteration
-            train_loader = create_housing_data_loader(train_data, batch_size=batch_size, shuffle=True)
+        except StopIteration:
+            breakpoint()
+            train_loader = create_housing_data_loader(train_data, target=target, batch_size=batch_size, shuffle=True)
             curr_epoch_loss = batch_loss
             epoch_losses.append(curr_epoch_loss)
+            print(f"Epoch: {epoch}, loss = {curr_epoch_loss:.4f}")
             
             if abs(prev_epoch_loss - curr_epoch_loss) < error:
                 model, epoch_losses
@@ -49,15 +58,21 @@ def train_model(train_data, model, loss, optimizer, batch_size=64, error=10e-4):
             
             epoch += 1
             continue
-
-        X, y = train_batch[train_batch.columns[:-1]], train_batch[train_batch.columns[-1]]
-        y_pred = model(X)
-
-        batch_loss += loss(y, y_pred)
-        batch_loss.backward()
-        optimizer.step()
         
 
 
 if __name__ == "__main__":
-    pass
+    from utils.load_data import load_housing_data
+    train_data, testing_data = load_housing_data()
+
+    num_train_cols = train_data.shape[1]-1
+    model1 = RegressionModel(input_dim=num_train_cols, hidden_dim=num_train_cols//2, output_dim=1)
+
+
+    mse_loss = nn.MSELoss()
+    adam_optimizer = optim.Adam(model1.parameters())
+
+
+
+
+    model1, losses = train_model(train_data=train_data, target="median_house_value", model=model1, loss=mse_loss, optimizer=adam_optimizer)
