16aa17abb7104cc09c50627077ce158871d55d99
Callihan Bertley <callihanbertley@Callihans-MacBook-Pro-2.local>
2025-03-22 20:53:50 -0500

    Added function to display image

--- utils/data_preprocessing.py
+++ utils/data_preprocessing.py
@@ -1,6 +1,7 @@
 import torch
 from torch.utils.data import DataLoader, sampler
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class HousingDataLoader():
@@ -72,6 +73,16 @@ def create_fashion_dataloaders(train_data, test_data, val_size=0.2, batch_size=6
     return train_data_loader, None, test_data_loader
 
 
+def show_clothing(dataloader, ind=0):
+    for _ in range(ind+1):
+        image, label = next(iter(dataloader))
+    
+    classes = ['T-shirt/top','Trouser','Pullover','Dress','Coat','Sandal','Shirt','Sneaker','Bag','Ankle Boot']
+
+    print(classes[label[0].item()])
+    plt.title(classes[label[0].item()])
+    plt.imshow(image[0].numpy().squeeze(), cmap='gray')
+    plt.show()
 
 if __name__ == "__main__":
     from load_data import *
@@ -106,3 +117,5 @@ if __name__ == "__main__":
     print(type(train_loader))
     print(type(val_loader))
     print(type(test_loader))
+
+    show_clothing(dataloader=train_loader, ind=0)
