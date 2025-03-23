5ea3986463024c9c6d3fe8bc1cf80c4beafe2f89
Callihan Bertley <callihanbertley@Callihans-MacBook-Pro-2.local>
2025-03-22 20:42:09 -0500

    Added functions to create dataloaders for FashionMNIST dataset

--- utils/data_preprocessing.py
+++ utils/data_preprocessing.py
@@ -1,5 +1,6 @@
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, sampler
+import numpy as np
 
 
 class HousingDataLoader():
@@ -36,17 +37,53 @@ def create_housing_data_loader(data, target, batch_size=64, shuffle=True):
     return data_loader
 
 
+def train_validation_split(train_data_size, val_size):
+    assert  0 < val_size < 1, "val_size must be between 0 and 1"
+    train_size = 1 - val_size
+    
+    indices = list(range(train_data_size))
+    np.random.shuffle(indices)
+    split = int(np.floor(train_size * train_data_size))
+    train_indices, val_indices = indices[:split], indices[split:]
+    return train_indices, val_indices
+
+def create_fashion_dataloaders(train_data, test_data, val_size=0.2, batch_size=64):
+    if val_size:
+        train_size = len(train_data)
+        train_indices, val_indices = train_validation_split(train_size, val_size=val_size)
+
+
+        train_sampler = sampler.SubsetRandomSampler(train_indices)
+        val_sampler = sampler.SubsetRandomSampler(val_indices)
+
+        train_data_loader = DataLoader(train_data, batch_size=batch_size, sampler=train_sampler)
+
+        val_data_loader = DataLoader(train_data, batch_size=batch_size, sampler=val_sampler)
+
+        test_data_loader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
+
+        return train_data_loader, val_data_loader, test_data_loader
+
+
+    train_data_loader = DataLoader(train_data, batch_size)
+
+    test_data_loader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
+
+    return train_data_loader, None, test_data_loader
+
+
+
 if __name__ == "__main__":
     from load_data import *
 
-    housing_train_data, housing_test_data = load_housing_data()
-
-    housing_traind_data_loader = create_housing_data_loader(housing_train_data, "median_house_value")
+    # Housing Dataset
+    train_data, test_data = load_housing_data()
+    train_loader = create_housing_data_loader(train_data, "median_house_value")
 
     show = True
     while True:
         try: 
-            X, y = next(housing_traind_data_loader)
+            X, y = next(train_loader)
             if show:
                 print(X, y)
                 show = False
@@ -55,3 +92,17 @@ if __name__ == "__main__":
             break
 
     print("Iterated through all batches")
+
+    # Fashion MNIST Dataset
+    train_data, test_data = load_fashion()
+
+    train_loader, val_loader, test_loader = create_fashion_dataloaders(
+      train_data=train_data,
+      test_data=test_data,
+      val_size=0.2,
+      batch_size=64  
+    )
+
+    print(type(train_loader))
+    print(type(val_loader))
+    print(type(test_loader))
