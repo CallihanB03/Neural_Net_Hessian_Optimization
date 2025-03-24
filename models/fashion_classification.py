5ff0626375c369dcadc3064c0b7573dcbd648bce
Callihan Bertley <callihanbertley@wireless-10-148-10-188.public.utexas.edu>
2025-03-23 21:14:40 -0500

    Added function to train simple fashion classifier

--- models/fashion_classification.py
+++ models/fashion_classification.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
 from torchsummary import summary
+import torch.optim as optim
+from utils.load_data import load_fashion
+from utils.data_preprocessing import create_fashion_dataloaders
 
 class Simple_classifier(nn.Module):
     def __init__(self, input_dim, 
@@ -99,12 +102,77 @@ class CNN_classifier(nn.Module):
         x = self.relu(self.fc3(x))
         x = self.logsoftmax(self.fc4(x))
 
-
-
-
     
+def train_simple_classifier(model, loss_fn, optimizer, error, train_loader, val_loader, device):
+    train_losses = []
+    val_losses = []
+    prev_epoch_loss = float("inf")
+    epoch = 1
+
+    while True:
+        model.train()
+        train_epoch_loss = 0
+
+        for images, labels in train_loader:
+            images, labels = images.to(device), labels.to(device)
+            images = images.view(images.shape[0], -1)
+            train_preds = model(images)
+            train_batch_loss = loss_fn(train_preds, labels)
+            optimizer.zero_grad()
+            train_batch_loss.backward()
+            optimizer.step()
+            train_epoch_loss += train_batch_loss.item()
+
+        else:
+            train_epoch_loss = train_batch_loss / len(train_loader)
+
+            with torch.no_grad():
+                val_epoch_acc = 0
+                val_epoch_loss = 0
+                model.eval()
+
+                for images, labels in val_loader:
+                    images, labels = images.to(device), labels.to(device)
+                    images = images.view(images.shape[0], -1)
+                    val_preds = model(images)
+                    val_batch_loss = loss_fn(val_preds, labels)
+                    val_epoch_loss += val_batch_loss.item()
+
+                    proba = torch.exp(val_preds)
+                    _, pred_labels = proba.topk(1, dim=1)
+
+                    result = pred_labels == labels.view(pred_labels.shape)
+                    batch_acc = torch.mean(result.type(torch.FloatTensor))
+                    val_epoch_acc += batch_acc.item()
+
+                else:
+                    val_epoch_loss = val_epoch_loss / len(val_loader)
+                    val_epoch_acc = val_epoch_acc / len(val_loader)
+                    train_losses.append(train_epoch_loss)
+                    val_losses.append(val_epoch_loss)
+
+                    print(f"Epoch: {epoch} -> train_loss: {train_epoch_loss:.6f}, val_loss = {val_epoch_loss:.6f}, val_acc: {val_epoch_acc*100:.4f}%")
+        
+
+        if abs(train_epoch_loss - prev_epoch_loss) < error:
+            return model, train_losses, val_losses
+        
+        epoch += 1
+        prev_epoch_loss = train_epoch_loss
+
+
 
 if __name__ == "__main__":
+    # Fashion MNIST Dataset
+    train_data, test_data = load_fashion()
+
+    train_loader, val_loader, test_loader = create_fashion_dataloaders(
+      train_data=train_data,
+      test_data=test_data,
+      val_size=0.2,
+      batch_size=64  
+    )
+
 
     # Input Shape 64 x 784
     simple_classifier = Simple_classifier(
@@ -116,21 +184,39 @@ if __name__ == "__main__":
         output_dim=10,
         dropout_rate=0.25
     )
-    simple_classifier_summary = summary(simple_classifier, (64, 784))
 
-    # Input Shape 1 x 28 x 28
-    cnn_classifier = CNN_classifier(
-        input_dim=1,
-        hidden_conv_dim1=32,
-        hidden_conv_dim2=64,
-        hidden_conv_dim3=64,
-        hidden_linear_dim1=250,
-        hidden_linear_dim2=125,
-        hidden_linear_dim3=60,
-        output_dim=10,
-        kernel_size=3,
-        pool_size=2
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    loss_fn = nn.NLLLoss()
+    optimizer = optim.Adam(simple_classifier.parameters(), lr=0.001, weight_decay=0.)
+
+    simple_classifier, train_losses, val_losses = train_simple_classifier(
+        model=simple_classifier,
+        loss_fn=loss_fn,
+        optimizer=optimizer,
+        error=10e-6,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        device=device
     )
 
-    cnn_classifier_summary = summary(cnn_classifier, (1, 28, 28))
+
+    # simple_classifier_summary = summary(simple_classifier, (64, 784))
+
+    # Input Shape 1 x 28 x 28
+    # cnn_classifier = CNN_classifier(
+    #     input_dim=1,
+    #     hidden_conv_dim1=32,
+    #     hidden_conv_dim2=64,
+    #     hidden_conv_dim3=64,
+    #     hidden_linear_dim1=250,
+    #     hidden_linear_dim2=125,
+    #     hidden_linear_dim3=60,
+    #     output_dim=10,
+    #     kernel_size=3,
+    #     pool_size=2
+    # )
+
+    # cnn_classifier_summary = summary(cnn_classifier, (1, 28, 28))
+
 
