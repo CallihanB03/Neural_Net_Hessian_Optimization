1545c9842bbeec40fc63ef6d501ddd6082add15e
Callihan Bertley <callihanbertley@wireless-10-148-10-188.public.utexas.edu>
2025-03-23 22:19:21 -0500

    Fixed path bug

--- utils/create_plots.py
+++ utils/create_plots.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import os
 
 def show_clothing(dataloader, ind=0):
     for _ in range(ind+1):
@@ -13,3 +15,20 @@ def show_clothing(dataloader, ind=0):
     plt.close()
 
 
+def plot_training_and_validation_loss(train_losses, val_losses, y_label, show, save, relative_save_path):
+    if save and not relative_save_path:
+        assert ValueError, "must have value for argument save_path if save=True"
+
+    epochs = np.arange(1, len(train_losses)+1)
+    plt.plot(epochs, train_losses)
+    plt.scatter(epochs, val_losses, color="red")
+    plt.legend(["Train Loss", "Validation Loss"])
+    plt.xlabel("Epochs")
+    plt.ylabel(y_label)
+    if save:
+        plt.savefig("".join([os.getcwd(), relative_save_path]))
+
+    if show:
+        plt.show()
+
+    plt.close()
